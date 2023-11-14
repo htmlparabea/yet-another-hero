31bc42e6aeb97ecb0a90ea6f9dee802a00c10d5c
htmlparabea <htmlparabea@gmail.com>
2023-11-14 22:30:57 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let sketch = (p) => {
+
+    p.preload = () => {
+
+    };
+
+    p.setup = () => {
+    };
+
+    p.mouseClicked = () => {
+
+    };
+
+    p.keyPressed = () => {
+
+    };
+
+    p.draw = () => {
+
+    };
+
+}
+
+let myp5 = new p5(sketch);
